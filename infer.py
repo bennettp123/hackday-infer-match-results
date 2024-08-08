9d77b4a53b8fd9dc4f8aa593c91a804758411134
Bennett Perkins <1610227+bennettp123@users.noreply.github.com>
2024-08-08 10:08:09 +0800

    submit as csv instead of json (allows multiple predictions in a single request)

--- infer.py
+++ infer.py
@@ -1,6 +1,7 @@
 import boto3
 import argparse
 import json
+import pandas as pd
 
 
 # only used when --enpoint-name is omitted
@@ -10,34 +11,45 @@ DEFAULT_ENDPOINT = \
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-
     parser.add_argument('--home-team-id', type=int, required=True)
     parser.add_argument('--away-team-id', type=int, required=True)
     parser.add_argument('--venue-id', type=int, required=True)
     parser.add_argument('--endpoint-name', type=str, default=DEFAULT_ENDPOINT)
-
     args = parser.parse_args()
 
-    client = boto3.client('runtime.sagemaker')
+    sagemaker_runtime = boto3.client('runtime.sagemaker')
 
-    body = {
-        "features": [
-            int(args.home_team_id),
-            int(args.away_team_id),
-            int(args.venue_id),
-        ]
-    }
+    # prepare request data
+    records = [
+        {
+            'home_team_id': args.home_team_id,
+            'away_team_id': args.away_team_id,
+            'venue_id': args.venue_id,
+        },
+        # ... more records (optional)
+    ]
+    
+    # convert to csv
+    data = pd.DataFrame.from_records(records)
+    body = data.to_csv(
+        columns=['home_team_id', 'away_team_id', 'venue_id'],
+        index=False,
+        header=False,
+    )
 
-    response = client.invoke_endpoint(
+    # invoke model to get predictions
+    response = sagemaker_runtime.invoke_endpoint(
         EndpointName=args.endpoint_name,
-        ContentType="application/json",
-        Body=bytes(json.dumps(body), encoding='utf-8'),
+        ContentType="text/csv",
+        Body=bytes(body, encoding='utf-8'),
         Accept="application/json"
     )
 
+    # parse response
     response_json = json.loads(response['Body'].read().decode('utf-8'))
     predictions = response_json['predictions']
 
+    # print predicted results
     for prediction in predictions:
         score = int(prediction['score'])
         if score == 0:
